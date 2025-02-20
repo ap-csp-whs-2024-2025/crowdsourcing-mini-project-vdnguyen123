cdc570009158a479b26d9a0c9f6230966ff13751
vdnguyen123 <vdnguyen123@student.hbuhsd.edu>
2025-02-20 18:14:24 +0000

    022025

--- crowdsourcing.cpp
+++ crowdsourcing.cpp
@@ -21,17 +21,17 @@ int main()
     std::string choice = "y"; // y or n to enter a new song
     while (choice != "n")
     {
-        std::cout << "\nEnter a song name\n";
+        std::cout << "\nEnter a song name (don't add spaces)\n";
         std::string name;
         std::cin >> name;
         song_names.push_back(name);
     
-        std::cout << "\nEnter the artist of the song\n";
+        std::cout << "\nEnter the artist of the song (no spaces)\n";
         std::string artist;
         std::cin >> artist;
         song_artist.push_back(artist);
     
-        std::cout << "\nEnter the duration of the song\n";
+        std::cout << "\nEnter the duration of the song (in seconds)\n";
         int duration;
         std::cin >> duration;
         song_duration.push_back(duration);
